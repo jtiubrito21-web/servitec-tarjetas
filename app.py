12d923d9b2195b2bc4b10c687ea5a5c72259066a
jtiubrito21-web <jtiubrito21@gmail.com>
2026-03-19 19:41:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def logged():
 
 @app.route("/")
 def inicio():
-    return redirect(url_for("nuevo"))
+    return redirect(url_for("login"))
  
 @app.route("/login", methods=["GET","POST"])
 def login():
